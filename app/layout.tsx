1e327f7253b0d85a4b88e9fa4ebab369abecaf14
websitesconvertads <websitesconvertads@gmail.com>
2025-12-05 12:58:31 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
+import "../styles/global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Find Best Realtors & Homes for Sale in Phoenix, AZ",
   description:
     "Explore homes for sale in Phoenix, AZ with top realtors. Browse new, luxury, mobile and multi-family properties and find your perfect home today.",
@@ -16,24 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta
-          name="description"
-          content="Explore homes for sale in Phoenix, AZ with top realtors. Browse new, luxury, mobile and multi-family properties and find your perfect home today. "
-        />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="www.davisrealtyteam.com" />
-        <meta property="og:title" content="Davis Realty Team" />
-        <meta
-          property="og:description"
-          content="Explore homes for sale in Phoenix, AZ with top realtors. Browse new, luxury, mobile and multi-family properties and find your perfect home today. "
-        />
-        <meta property="og:image" content="Your Image URL" />
-        <link rel="icon" href="/images/favicon.ico" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
