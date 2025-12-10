0df91850b1b7a7d29041fc96eef9e075a6a62282
websitesconvertads <websitesconvertads@gmail.com>
2025-12-10 16:25:32 +0530

    Update Footer.tsx

--- Pages/Footer/Footer.tsx
+++ Pages/Footer/Footer.tsx
@@ -97,19 +97,7 @@ const Footer = () => {
     margin: "20px 0 0 0",
   }}
 >
-  <a href="https://www.davisrealtyteam.com/realtors-in-phoenix-az">Realtors in Phoenix</a> | 
-  <a href="https://www.davisrealtyteam.com/homes-for-sale-phoenix-az">Homes for Sale Phoenix</a> | 
-  <a href="https://www.davisrealtyteam.com/first-time-homebuyer-phoenix-az">First Time Homebuyer Phoenix</a> |
-  <a href="https://www.davisrealtyteam.com/realtors-in-peoria-az">Realtors in Peoria</a> |  
-  <a href="https://www.davisrealtyteam.com/homes-for-sale-peoria-az"> Homes for Sale Peoria</a> | 
-  <a href="https://www.davisrealtyteam.com/homes-for-sale-anthem-az">Homes for Sale Anthem</a> | 
-  <a href="https://www.davisrealtyteam.com/realtors-in-surprise-az">Realtors in Surprise</a> | 
-  <a href="https://www.davisrealtyteam.com/homes-for-sale-surprise-az">Homes for Sale Surprise</a> | 
-  <a href="https://www.davisrealtyteam.com/realtors-in-scottsdale-az">Realtors in Scottsdale</a> | 
-  <a href="https://www.davisrealtyteam.com/homes-for-sale-scottsdale-az">Homes for Sale Scottsdale</a> | 
-  <a href="https://www.davisrealtyteam.com/homes-for-sale-gilbert-az">Homes for Sale Gilbert</a> | 
-  <a href="https://www.davisrealtyteam.com/realtors-in-gilbert-az">Realtors in Gilbert</a> | 
-  <a href="https://www.davisrealtyteam.com/areas-we-serve">Areas We Serve</a>
+  <a href="https://www.davisrealtyteam.com/realtors-in-phoenix-az">Realtors in Phoenix</a> | <a href="https://www.davisrealtyteam.com/homes-for-sale-phoenix-az">Homes for Sale Phoenix</a> | <a href="https://www.davisrealtyteam.com/first-time-homebuyer-phoenix-az">First Time Homebuyer Phoenix</a> | <a href="https://www.davisrealtyteam.com/realtors-in-peoria-az">Realtors in Peoria</a> |  <a href="https://www.davisrealtyteam.com/homes-for-sale-peoria-az"> Homes for Sale Peoria</a> | <a href="https://www.davisrealtyteam.com/homes-for-sale-anthem-az">Homes for Sale Anthem</a> | <a href="https://www.davisrealtyteam.com/realtors-in-surprise-az">Realtors in Surprise</a> | <a href="https://www.davisrealtyteam.com/homes-for-sale-surprise-az">Homes for Sale Surprise</a> | <a href="https://www.davisrealtyteam.com/realtors-in-scottsdale-az">Realtors in Scottsdale</a> | <a href="https://www.davisrealtyteam.com/homes-for-sale-scottsdale-az">Homes for Sale Scottsdale</a> | <a href="https://www.davisrealtyteam.com/homes-for-sale-gilbert-az">Homes for Sale Gilbert</a> | <a href="https://www.davisrealtyteam.com/realtors-in-gilbert-az">Realtors in Gilbert</a> | <a href="https://www.davisrealtyteam.com/areas-we-serve">Areas We Serve</a>
 </p>
 
       </div>
