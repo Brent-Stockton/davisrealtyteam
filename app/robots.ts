25d7c059edc2273b0849a1c962e2f10b7d0d3b49
websitesconvertads <websitesconvertads@gmail.com>
2025-12-16 15:03:31 +0530

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/cgi-bin/"],
+      },
+    ],
+    sitemap: "https://www.davisrealtyteam.com/sitemap.xml",
+  };
+}
